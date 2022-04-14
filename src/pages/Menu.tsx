f96bda9354159817eb988382b64a2188c15fb491
Fujinoka <pujisaefuloh@gmail.com>
2022-04-14 14:23:18 +0700

    add feature to menu page

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,7 @@
 import {
+  IonBackButton,
     IonButton,
+    IonButtons,
     IonCard,
     IonCardContent,
     IonCardHeader,
@@ -8,6 +10,7 @@ import {
     IonCol,
     IonContent,
     IonGrid,
+    IonHeader,
     IonIcon,
     IonInput,
     IonItem,
@@ -15,6 +18,8 @@ import {
     IonPage,
     IonRow,
     IonText,
+    IonTitle,
+    IonToolbar,
   } from "@ionic/react";
   import { useState } from "react";
 
@@ -24,56 +29,72 @@ import {
   
     return (
       <IonPage>
-        <IonContent fullscreen className="ion-padding container">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonBackButton defaultHref="home" />
+            </IonButtons>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen className="ion-padding">
           <IonGrid>
             <IonRow>
               <IonCol className="ion-text-center">
-              <IonCard>
-          <IonCardHeader>
-            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-            <IonCardTitle>Card Title</IonCardTitle>
-          </IonCardHeader>
-
-          <IonCardContent>
-            Keep close to Nature's heart... and break clear away, once in awhile,
-            and climb a mountain or spend a week in the woods. Wash your spirit clean.
-      </IonCardContent>
-        </IonCard>
-
-        <IonCard>
-          <IonItem>
-            <IonIcon icon={pin} slot="start" />
-            <IonLabel>ion-item in a card, icon left, button right</IonLabel>
-            <IonButton fill="outline" slot="end">View</IonButton>
-          </IonItem>
-
-          <IonCardContent>
-            This is content, without any paragraph or header tags,
-            within an ion-cardContent element.
-      </IonCardContent>
-        </IonCard>
-
-        <IonCard>
-          <IonItem href="#" className="ion-activated">
-            <IonIcon icon={wifi} slot="start" />
-            <IonLabel>Card Link Item 1 activated</IonLabel>
-          </IonItem>
-
-          <IonItem href="#">
-            <IonIcon icon={wine} slot="start" />
-            <IonLabel>Card Link Item 2</IonLabel>
-          </IonItem>
-
-          <IonItem className="ion-activated">
-            <IonIcon icon={warning} slot="start" />
-            <IonLabel>Card Button Item 1 activated</IonLabel>
-          </IonItem>
-
-          <IonItem>
-            <IonIcon icon={walk} slot="start" />
-            <IonLabel>Card Button Item 2</IonLabel>
-          </IonItem>
-        </IonCard> 
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>Menu</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quia delectus corrupti? Tenetur ut architecto atque hic odio quasi
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-text-center">
+              <IonCol size="6">
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>Steak</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    IDR. 30.000
+                    <IonButton size="small" color="warning" expand="full">+</IonButton>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol size="6">
+                <IonCard>
+                    <IonCardHeader>
+                      <IonCardTitle>Ramen</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                      IDR. 45.000
+                    <IonButton size="small" color="warning" expand="full">+</IonButton>
+                    </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol size="6">
+                <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle>BoBa</IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    IDR. 23.000
+                    <IonButton size="small" color="warning" expand="full">+</IonButton>
+                  </IonCardContent>
+                </IonCard>
+              </IonCol>
+              <IonCol size="6">
+                <IonCard>
+                    <IonCardHeader>
+                      <IonCardTitle>Menu</IonCardTitle>
+                    </IonCardHeader>
+                    <IonCardContent>
+                      IDR. 14.000
+                    <IonButton size="small" color="warning" expand="full">+</IonButton>
+                    </IonCardContent>
+                </IonCard>
               </IonCol>
             </IonRow>
           </IonGrid>
